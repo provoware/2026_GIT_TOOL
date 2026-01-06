c1b9384220bb2cd3dcc7caed3a61e6bcf2ae8829
karsten tschiersch <provoware.one@gmail.com>
2026-01-06 04:40:55 +0100

    Add startup routine with IPC status updates

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,18 @@ export default [
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        document: "readonly",
+        window: "readonly",
+        navigator: "readonly",
+        crypto: "readonly",
+        setInterval: "readonly"
+      }
     },
     rules: {
       "no-console": "off"
